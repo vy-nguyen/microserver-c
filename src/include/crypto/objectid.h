6580a072ea39c21b038d737efcb84f31d1ae83f6
Vy Nguyen <vynguyen@tvntd.com>
2024-10-30 23:08:52 -0700

    Fix unit test.

--- src/include/crypto/objectid.h
+++ src/include/crypto/objectid.h
@@ -1,6 +1,7 @@
 
 #pragma once
 
+#include <iostream>
 #include <cctype>
 #include <string>
 #include <cstring>
@@ -25,13 +26,18 @@ class ObjectId {
 
     ObjectId(const std::string &hex) {
         const char *str = hex.c_str();
-        size_t limit = std::min(2 * sizeof(m_data), hex.size());
+        if (str[0] == '0' && (str[1] == 'x' || str[1] == 'X')) {
+            str += 2;
+        }
+        size_t limit = std::min(2 * sizeof(m_data), std::strlen(str));
 
         for (size_t i = 0; i < limit; i += 2) {
+            auto index = (i >> 1);
             if (!std::isxdigit(str[i]) && !std::isxdigit(str[i + 1])) {
+                m_data[index] = '\0';
                 continue;
             }
-            sscanf(str + i, "%2hhx", m_data + (i >> 1));
+            sscanf(str + i, "%2hhx", m_data + index);
         }
     }
 
@@ -68,13 +74,18 @@ class ObjectId {
         return true;
     }
 
+    ObjectId &assign(const unsigned char *const buf) {
+        std::copy(buf, buf + sizeof(m_data), m_data);
+        return *this;
+    }
+
     std::string to_string() const {
         constexpr auto size = (SHA_DIGEST_LENGTH * 2);
         char hex[size + 1];
 
         hex[size] = '\0';
         for (size_t i = 0; i < sizeof(m_data); i++) {
-            std::sprintf(hex + (i << 2), "%02x", m_data[i]);
+            std::sprintf(hex + (i << 1), "%02x", m_data[i]);
         }
         return std::string(hex);
     }
--- tests/test_objectid.cpp
+++ tests/test_objectid.cpp
@@ -1,8 +1,40 @@
 #include <gtest/gtest.h>
 #include "crypto/objectid.h"
 
+using namespace seal;
+
+void objid_ctor(const std::string &hex) {
+    ObjectId o1 = ObjectId(hex);
+
+    if (hex.rfind(std::string("0x"), 0) == 0 ||
+        hex.rfind(std::string("0X"), 0) == 0) {
+        EXPECT_EQ(hex.substr(2), o1.to_string());
+    } else {
+        EXPECT_EQ(hex, o1.to_string());
+    }
+
+    ObjectId o2 = ObjectId(o1);
+    EXPECT_EQ(o1, o2);
+
+    ObjectId o3 = o1;
+    EXPECT_EQ(o3, o2);
+
+    std::string value = o2.to_string();
+    ObjectId o4 = ObjectId(value.c_str());
+    EXPECT_EQ(o4, o1);
+
+    ObjectId o5 = ObjectId(std::move(o1));
+    EXPECT_EQ(o5, o2);
+
+    // ObjectId o6 = o1;
+    // EXPECT_EQ(o6, o2);
+}
+
 TEST(ObjectIdTest, ConstructorTest) {
     seal::ObjectId obj;
 
     EXPECT_EQ(obj, seal::ObjectId::ZeroID);
+    objid_ctor(std::string("0d57542aafc1f62d86e2071da30686e4c324e4f0"));
+    objid_ctor(std::string("0x0d57542aafc1f62d86e2071da30686e4c324e4f0"));
+    objid_ctor(std::string("0X0d57542aafc1f62d86e2071da30686e4c324e4f0"));
 }
