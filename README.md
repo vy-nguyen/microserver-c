52e76df154d53e2d99f0c8e7ffc9123749808c98
Vy Nguyen <vy.workplan@gmail.com>
2024-12-06 23:23:09 +0000

    Update fresh build dependencies.

---
+++ README.md
@@ -0,0 +1,4 @@
+Fix the build:
+
+- Comment out boost_json dependency on jwt-cpp subproject
+- Comment out soci/include/soci_platform.h error on c++-11 check.
--- scripts/predepend.sh
+++ scripts/predepend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo apt install -y cmake g++ libssl-dev libboost-dev
-sudo apt install -y libboost-system-dev libboost-filesystem-dev
+sudo apt install -y cmake g++ libssl-dev libboost-dev meson
+sudo apt install -y libboost-system-dev libboost-filesystem-dev libboost-json-dev 
 sudo apt install -y libmysqlcppconn-dev
 sudo apt install -y libmysqlclient-dev
 sudo apt install -y openapi-generator-cli
